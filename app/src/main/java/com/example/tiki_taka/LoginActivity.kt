64b87b322fb393fe23e57242f8ef212f8286391a
jiheunkim <ziheun54@gmail.com>
2023-05-22 01:18:37 +0900

    heun

---
+++ app/src/main/java/com/example/tiki_taka/LoginActivity.kt
@@ -0,0 +1,50 @@
+package com.example.tiki_taka
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.tiki_taka.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        init()
+    }
+
+    private fun init() {
+        val id = binding.editID.text.toString()
+        val passwd = binding.editPasswd.text.toString()
+
+        //로그인 버튼
+        binding.btnLogin.setOnClickListener {
+            login(id, passwd)
+            if (id.isEmpty() && passwd.isEmpty()) {
+                Toast.makeText(this, "Enter your id and password.", Toast.LENGTH_SHORT).show()
+            } else {
+                //login(id, passwd)
+            }
+        }
+
+        //회원가입 화면 인텐트
+        binding.signin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        //비밀번호 찾기 화면 인텐트
+        binding.findpasswd.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun login(id: String, passwd: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
