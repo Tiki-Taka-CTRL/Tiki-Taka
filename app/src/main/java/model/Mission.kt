a5ccd7905e5f82bc6a3344a8f81d3b6fa309b5f6
Park99 <sungung1030@gmail.com>
2023-05-27 18:06:51 +0900

    실행되도록 수정완료(ChattingRvAdapter주석처리)

--- app/src/main/java/model/Mission.kt
+++ app/src/main/java/model/Mission.kt
@@ -2,6 +2,7 @@ package model
 
 import java.io.Serializable
 
-data class Mission(val level: Int,
-                   val contents: String,
-                   ): Serializable
+data class Mission(
+    val level: Int,
+    val contents: String,
+) : Serializable
