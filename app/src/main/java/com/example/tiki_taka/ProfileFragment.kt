5d155e4d62535045a3ab1161e3b51dd98864cea7
binllionaire <bug20474@gmail.com>
2023-06-04 03:27:11 +0900

    publish : 아바타꾸미기 화면 퍼블리싱

--- app/src/main/java/com/example/tiki_taka/ProfileFragment.kt
+++ app/src/main/java/com/example/tiki_taka/ProfileFragment.kt
@@ -1,10 +1,10 @@
 package com.example.tiki_taka
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.tiki_taka.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -16,7 +16,13 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentProfileBinding.inflate(layoutInflater)
+       // init()
         return binding.root
     }
+    fun init() {
+        binding.avatar1.setOnClickListener{
+        }
 
-}
+    }
+
+    }
